ec55b0fb11821c251b2bd47bd485404204f9e056
Carterefe <abdenniz1@gmail.com>
2025-02-24 21:21:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=YsRUEL5R#L-Gdtpa7uZbdKF4FSAy_vjTh7noigBrtXTyechcL07s' : process.env.SESSION_ID,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'carterefe' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '2349165278728' : process.env.OWNER_NUMBER
 };
